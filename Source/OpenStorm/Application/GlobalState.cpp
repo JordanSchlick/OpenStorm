3806db72602bf9efb9c585b20d16f5410156bac1
JordanSchlick <91904908+JordanSchlick@users.noreply.github.com>
2022-08-08 23:46:03 -0500

    Created GlobalState

---
+++ Source/OpenStorm/Application/GlobalState.cpp
@@ -0,0 +1,8 @@
+#include "GlobalState.h"
+
+#include <stdio.h>
+
+void GlobalState::test() {
+	fprintf(stderr, "testFloat %f\n", testFloat);
+}
+
---
+++ Source/OpenStorm/Application/GlobalState.h
@@ -0,0 +1,9 @@
+
+
+class GlobalState{
+public:
+	float testFloat = 0;
+	void test();
+private:
+	
+};
