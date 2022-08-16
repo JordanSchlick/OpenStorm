c50d93649765df930db1ddad7d58a25e0391b11d
Cryptic-Sailor <23349349+Cryptic-Sailor@users.noreply.github.com>
2022-08-16 17:19:00 -0500

    Added Unlinked Global variables & UI Changes

--- Source/OpenStorm/Application/GlobalState.h
+++ Source/OpenStorm/Application/GlobalState.h
@@ -2,10 +2,20 @@
 
 class GlobalState{
 public:
-	bool inputToggle = false;
-	float testFloat = 0;
-	float moveSpeed = 300.0f;
-	void test();
+	bool inputToggle = false; // toggle mouse input
+	bool fade = false; // animate fade
+	bool animate = false; // animate the scene
+	bool interpolation = false; // interpolate the scene
+	
+	float animateSpeed = 0.0f; // speed of animation
+	float fadeSpeed = 0.0f; // speed of fade
+	float moveSpeed = 300.0f; // speed of movement
+	float rotateSpeed = 200.0f; // speed of rotation
+
+	
+	//Testing
+	float testFloat = 0; // test float
+	void test(); // test fuction
 private:
 	
 };
